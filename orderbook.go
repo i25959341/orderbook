faefca37df98ffae0c402203f8f9d02e6885aa66
i25959341 <karlson.lee09@gmail.com>
2018-04-25 09:47:32 +0100

    finish 1st draft

--- orderbook.go
+++ orderbook.go
@@ -40,7 +40,92 @@ func (orderBook *OrderBook) WorstAsk() (value interface{}, found bool) {
 	return value, found
 }
 
-func (orderBook *OrderBook) ProcessOrderList(side string, orderList OrderList, quantityStillToTrade decimal.Decimal, quote map[string]string, verbose bool) (decimal.Decimal, []map[string]string) {
+func (orderBook *OrderBook) ProcessMarketOrder(quote map[string]string, verbose bool) []map[string]string {
+	var trades []map[string]string
+	quantity_to_trade, _ := decimal.NewFromString(quote["quantity"])
+	side := quote["side"]
+	var new_trades []map[string]string
+
+	if side == "bid" {
+		for quantity_to_trade.GreaterThan(decimal.Zero) && orderBook.asks.Length() > 0 {
+			best_price_asks := orderBook.asks.MinPriceList()
+			quantity_to_trade, new_trades = orderBook.ProcessOrderList("ask", best_price_asks, quantity_to_trade, quote, verbose)
+			trades = append(trades, new_trades...)
+		}
+
+	} else if side == "ask" {
+		for quantity_to_trade.GreaterThan(decimal.Zero) && orderBook.bids.Length() > 0 {
+			best_price_bids := orderBook.bids.MaxPriceList()
+			quantity_to_trade, new_trades = orderBook.ProcessOrderList("bid", best_price_bids, quantity_to_trade, quote, verbose)
+			trades = append(trades, new_trades...)
+		}
+	}
+	return trades
+}
+
+func (orderBook *OrderBook) ProcessLimitOrder(quote map[string]string, verbose bool) ([]map[string]string, map[string]string) {
+	var trades []map[string]string
+	quantity_to_trade, _ := decimal.NewFromString(quote["quantity"])
+	side := quote["side"]
+	price, _ := decimal.NewFromString(quote["price"])
+	var new_trades []map[string]string
+
+	var order_in_book map[string]string
+
+	if side == "bid" {
+		minPrice, _ := orderBook.asks.MinPrice()
+		for quantity_to_trade.GreaterThan(decimal.Zero) && orderBook.asks.Length() > 0 && price.GreaterThanOrEqual(minPrice.(decimal.Decimal)) {
+			best_price_asks := orderBook.asks.MinPriceList()
+			quantity_to_trade, new_trades = orderBook.ProcessOrderList("ask", best_price_asks, quantity_to_trade, quote, verbose)
+			trades = append(trades, new_trades...)
+			minPrice, _ = orderBook.asks.MinPrice()
+		}
+
+		if quantity_to_trade.GreaterThan(decimal.Zero) {
+			quote["order_id"] = strconv.Itoa(orderBook.next_order_id)
+			quote["quantity"] = quantity_to_trade.String()
+			orderBook.bids.InsertOrder(quote)
+			order_in_book = quote
+		}
+
+	} else if side == "ask" {
+		maxPrice, _ := orderBook.bids.MaxPrice()
+		for quantity_to_trade.GreaterThan(decimal.Zero) && orderBook.bids.Length() > 0 && price.LessThanOrEqual(maxPrice.(decimal.Decimal)) {
+			best_price_bids := orderBook.bids.MaxPriceList()
+			quantity_to_trade, new_trades = orderBook.ProcessOrderList("bid", best_price_bids, quantity_to_trade, quote, verbose)
+			trades = append(trades, new_trades...)
+			maxPrice, _ = orderBook.bids.MaxPrice()
+		}
+
+		if quantity_to_trade.GreaterThan(decimal.Zero) {
+			quote["order_id"] = strconv.Itoa(orderBook.next_order_id)
+			quote["quantity"] = quantity_to_trade.String()
+			orderBook.bids.InsertOrder(quote)
+			order_in_book = quote
+		}
+	}
+	return trades, order_in_book
+
+}
+
+func (orderBook *OrderBook) ProcessOrder(quote map[string]string, verbose bool) ([]map[string]string, map[string]string) {
+	order_type := quote["type"]
+	var order_in_book map[string]string
+	var trades []map[string]string
+
+	orderBook.UpdateTime()
+	quote["timestamp"] = strconv.Itoa(orderBook.time)
+	orderBook.next_order_id += 1
+
+	if order_type == "market" {
+		trades = orderBook.ProcessMarketOrder(quote, verbose)
+	} else {
+		trades, order_in_book = orderBook.ProcessLimitOrder(quote, verbose)
+	}
+	return trades, order_in_book
+}
+
+func (orderBook *OrderBook) ProcessOrderList(side string, orderList *OrderList, quantityStillToTrade decimal.Decimal, quote map[string]string, verbose bool) (decimal.Decimal, []map[string]string) {
 	quantityToTrade := quantityStillToTrade
 	var trades []map[string]string
 
@@ -91,76 +176,3 @@ func (orderBook *OrderBook) ProcessOrderList(side string, orderList OrderList, q
 	}
 	return quantityToTrade, trades
 }
-
-// def process_market_order(self, quote, verbose):
-// trades = []
-// quantity_to_trade = quote['quantity']
-// side = quote['side']
-// if side == 'bid':
-// 	while quantity_to_trade > 0 and self.asks:
-// 		best_price_asks = self.asks.min_price_list()
-// 		quantity_to_trade, new_trades = self.process_order_list('ask', best_price_asks, quantity_to_trade, quote, verbose)
-// 		trades += new_trades
-// elif side == 'ask':
-// 	while quantity_to_trade > 0 and self.bids:
-// 		best_price_bids = self.bids.max_price_list()
-// 		quantity_to_trade, new_trades = self.process_order_list('bid', best_price_bids, quantity_to_trade, quote, verbose)
-// 		trades += new_trades
-// else:
-// 	sys.exit('process_market_order() recieved neither "bid" nor "ask"')
-// return trades
-
-// def process_limit_order(self, quote, from_data, verbose):
-// order_in_book = None
-// trades = []
-// quantity_to_trade = quote['quantity']
-// side = quote['side']
-// price = quote['price']
-// if side == 'bid':
-// 	while (self.asks and price >= self.asks.min_price() and quantity_to_trade > 0):
-// 		best_price_asks = self.asks.min_price_list()
-// 		quantity_to_trade, new_trades = self.process_order_list('ask', best_price_asks, quantity_to_trade, quote, verbose)
-// 		trades += new_trades
-// 	# If volume remains, need to update the book with new quantity
-// 	if quantity_to_trade > 0:
-// 		if not from_data:
-// 			quote['order_id'] = self.next_order_id
-// 		quote['quantity'] = quantity_to_trade
-// 		self.bids.insert_order(quote)
-// 		order_in_book = quote
-// elif side == 'ask':
-// 	while (self.bids and price <= self.bids.max_price() and quantity_to_trade > 0):
-// 		best_price_bids = self.bids.max_price_list()
-// 		quantity_to_trade, new_trades = self.process_order_list('bid', best_price_bids, quantity_to_trade, quote, verbose)
-// 		trades += new_trades
-// 	# If volume remains, need to update the book with new quantity
-// 	if quantity_to_trade > 0:
-// 		if not from_data:
-// 			quote['order_id'] = self.next_order_id
-// 		quote['quantity'] = quantity_to_trade
-// 		self.asks.insert_order(quote)
-// 		order_in_book = quote
-// else:
-// 	sys.exit('process_limit_order() given neither "bid" nor "ask"')
-// return trades, order_in_book
-
-// def process_order(self, quote, from_data, verbose):
-//         order_type = quote['type']
-//         order_in_book = None
-//         if from_data:
-//             self.time = quote['timestamp']
-//         else:
-//             self.update_time()
-//             quote['timestamp'] = self.time
-//         if quote['quantity'] <= 0:
-//             sys.exit('process_order() given order of quantity <= 0')
-//         if not from_data:
-//             self.next_order_id += 1
-//         if order_type == 'market':
-//             trades = self.process_market_order(quote, verbose)
-//         elif order_type == 'limit':
-//             quote['price'] = Decimal(quote['price'])
-//             trades, order_in_book = self.process_limit_order(quote, from_data, verbose)
-//         else:
-//             sys.exit("order_type for process_order() is neither 'market' or 'limit'")
-//         return trades, order_in_book
